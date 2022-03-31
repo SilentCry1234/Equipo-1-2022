70916c99254f71975c2a7fc2e2f55941b42f3cf4
Davor Agnic <51179356+Deivor16@users.noreply.github.com>
2022-03-31 17:11:02 -0300

    Update MainMenu

--- Assets/UI/Scripts/MainMenu.cs
+++ Assets/UI/Scripts/MainMenu.cs
@@ -7,14 +7,23 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public Button playGame;
+    public Button options;
+    public Button quit;
+
     void Start()
     {
         playGame.onClick.AddListener(StartGame);
+        quit.onClick.AddListener(QuitGame);
     }
 
     void Update()
     {
-        
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 
     public void StartGame()
