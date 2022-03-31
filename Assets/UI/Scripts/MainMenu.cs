4dcd80b51dcf5530da13696a85897a84fe1b5092
Jesus Antonio Garcia Fusi <83846459+SilentCry1234@users.noreply.github.com>
2022-03-31 16:59:38 -0300

    Resulto problema al iniciar el juego

--- Assets/UI/Scripts/MainMenu.cs
+++ Assets/UI/Scripts/MainMenu.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Button playGame;
     void Start()
     {
-        Scene scene = SceneManager.GetActiveScene();
+        playGame.onClick.AddListener(StartGame);
     }
 
     void Update()
