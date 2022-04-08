8466c2260c1e065c43c312f29aeb89133d076beb
Stefano-bit <stefanogambinofornasin@hotmail.com>
2022-04-07 21:06:25 -0300

    Mejoras de Escenario

--- Assets/1.Personaje/Script/ControladorPersonaje.cs
+++ Assets/1.Personaje/Script/ControladorPersonaje.cs
@@ -9,6 +9,7 @@ public class ControladorPersonaje : MonoBehaviour
     public string nombre;
     public float speed;
     public bool enmantel;
+    public bool enmermelada;
 
     public static ControladorPersonaje instace; 
 
@@ -24,6 +25,10 @@ public class ControladorPersonaje : MonoBehaviour
             instace = this; 
         }
     }
+    private void Update()
+    {
+        Reducirvelocidad();
+    }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
@@ -31,11 +36,37 @@ public class ControladorPersonaje : MonoBehaviour
         {
             enmantel = true;
         }
+
+        if (collision.gameObject.CompareTag("Mermelada"))
+        {
+            enmermelada = true;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Mantel"))
+        {
             enmantel = false;
+        }
+
+        if (collision.gameObject.CompareTag("Mermelada"))
+        {
+            enmermelada = false;
+        }
     }
+
+    public void Reducirvelocidad()
+    {
+        if (enmermelada == true)
+        {
+            speed = 0f;
+        }
+
+        if (enmermelada == false)
+        {
+            speed = 7f;
+        }
+    }
+
 }
